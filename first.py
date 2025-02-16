f39d268e8df0a50eace63e1a6fa4978308f3bbaf
Tomás Liam Pertile <112728308+liampertile@users.noreply.github.com>
2025-02-15 23:31:31 -0300

    committed

--- first.py
+++ first.py
@@ -3,10 +3,8 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 
-
-x = np.array([200.0, 16.0]) # x is the vector with the inputs, so, it's a0 actually
-x = x.reshape(1, -1) # that array above is 1d, so with this, i create a 2d. Because keras with layers.Dense only admit 2d vectors
-layer_1 = layers.Dense(units=3, activation='sigmoid') # units means neurons, and the layer is the rectangle that we know from the lecture
+x = np.array([[200.0, 16.0]]) # x is the vector with the inputs, so, it's a0 actually  (NOTE, USE DOUBLE BRACKETS, IF YOU USE ONLY 1 PAIR OF THEM THEN, FOR PY, IS A 1D VECTOR AND TENSORFLOW DOESN'T LIKE IT, HE LIKE A MATRIX INTEAD.)
+layer_1 = layers.Dense(units=3, activation='sigmoid') # units means neurons, and the layer is the rectangle that we saw from the lecture
 a1= layer_1(x) #a1 is the result of activation, think about it in terms of probability! it's a vector of probabilities 
 
 layer_2 = layers.Dense(units=1, activation='sigmoid')
