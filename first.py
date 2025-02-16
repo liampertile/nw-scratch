d3151b0229179ef80c11bf8dbd19c501a5a43cc2
Tomás Liam Pertile <112728308+liampertile@users.noreply.github.com>
2025-02-16 19:29:34 -0300

    j

--- first.py
+++ first.py
@@ -17,11 +17,12 @@ x = np.array([[200.0, 16.0]])
 y = ... # some array
 
 model = Sequential([
-    layers.Dense(units=3,activation='sigmoid'),
+    layers.Dense(units=3,activation='relu'), #relu for hidden layers always, faster learning.
     layers.Dense(units=1,activation='sigmoid')
 ])
 
-model.compile(...)
-model.fit(...)
+model.compile(loss=...)  # this is where you select your cost function
+
+model.fit(X,y,epochs=n) # that's backpropagation, the epochs are the number of times of iterations of the gradient descent. Choose a good learning rate and follow the principles.
 
 model.predict(x_new)  # that's forward pass.
